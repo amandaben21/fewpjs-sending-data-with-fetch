e192db0199521eb01c92208de6de1ee0aae8f2cb
Amanda <amandaben21@aol.com>
2021-05-16 02:08:10 -0400

    commit tree 22b365e7f1b2c3a9456b36317fa5a59ee3647cd4

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    let msg = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        
+        body: JSON.stringify({
+            name,
+            email
+        })
+    };
+    
+    return fetch("http://localhost:3000/users", msg)
+    .then(responce => responce.json())
+    .then(resp => {
+        document.body.innerHTML = resp.id
+    })
+    
+    .catch(error => {
+      // const body = document.querySelector("body")
+      // body.innerText = error.message
+      document.body.innerHTML = error.message
+    })
+}
